f72fe06353f005c571e50d1aa4f3a8a406ff77b9
Guilherme Ayres <euaires.dev@gmail.com>
2026-04-22 18:36:10 -0300

    Calculadora de Média

---
+++ CALCULADORA DE MEDIA UNIFOR.js
@@ -0,0 +1,27 @@
+
+//Calculadora de média
+
+//declaradas
+let primeira_nota;
+let segunda_nota;
+let terceira_nota;
+
+//instanciar as variáveis
+primeira_nota = 10;
+segunda_nota = 6.5;
+terceira_nota = 8.2;
+
+
+let media;
+//expressão númerica
+media = (primeira_nota + segunda_nota + terceira_nota)/3;
+
+console.log(media);
+
+if(media > 7){
+    console.log("Parabéns, Você esta aprovado!");
+
+} else{
+    console.log("Você está reprovado, Estude mais!");
+
+}
---
+++ Calculadora CNH Unifor.js
@@ -0,0 +1,23 @@
+
+
+//Algoritimo para determinar se é possivel dirigir ou não!
+
+//idade >= 18
+//possui carteira de motorista
+
+//receber valor inteiro
+let idade;
+
+//possui carteira de motorista?
+let possui_cnh;
+
+idade = 18;
+possui_cnh = true;
+
+if (idade>=18 && possui_cnh == true){
+console.log("Você pode dirigir!");
+
+} else{
+console.log("Você ainda não pode dirigir!");
+
+}
