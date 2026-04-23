8d4a1d27c1d6dfcec7c5a64a2ad07c928dd625cf
Guilherme Ayres <euaires.dev@gmail.com>
2026-04-23 11:25:18 -0300

    Adicionando exercícios de condicionais

---
+++ Contador DoWhile.js
@@ -0,0 +1,9 @@
+//Aula de Estrutura de Repetição usando Do While
+
+let contador = 0;
+
+do{
+    console.log("Valor atual do contador: ", contador);
+    contador++;
+}while(contador <=100);
+
---
+++ ContadorFor.js
@@ -0,0 +1,6 @@
+//Aula de Estrutura de Repetição usando For
+
+
+for (i = 0; i <= 100; i++){
+    console.log("O valor: ", i);
+}
